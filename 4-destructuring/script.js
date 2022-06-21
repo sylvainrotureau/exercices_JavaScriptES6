05901e5b744295290ba98a4d8c6a2412a5f2a018
sylvain rotureau <syvlainrotureau@yahoo.fr>
2022-06-21 14:41:15 +0200

    echec exercice 4

--- 4-destructuring/script.js
+++ 4-destructuring/script.js
@@ -1,13 +1,31 @@
 //4.1 Découverte
 
+// déclaration d'une array avec comme valeur 1 pour la clé a et 2 pour la clé b
 const [a, b] = [1, 2, 3, 4]
 console.log(a)
 console.log(b)
 
-const {first, last} = {first: 'Paul', last: 'Henta', age: 35}
+// déclaration d'une array avec comme valeur 'paul' pour la clé first et 'henta' 
+// pour la clé last
+const {first, last, age} = {first: 'Paul', last: 'Henta', age: 35}
 console.log(first)
 console.log(last)
 
-//4.2 Application
+// 2. Afficher le 3e élément du tableau ainsi que la 
+// propriété `age` de l'objet
+console.log(age.key +': ' + age);
 
-console.log(data)
+// Pour chacunes des perturbations du fichier `data.js` 
+// (variable `data`) récupérer dans des variables puis afficher
+// leur texte, date de début et date de fin.
+
+  function info({texte: texte, dateDebut: dateStart, dateFin: dateEnd}){
+    Object.keys(data).forEach(key => {
+        console.log(data[key].texte);
+        console.log(data[key].dateDebut);
+        console.log(data[key].dateFin);
+        console.log("--------");
+      });
+    
+  }
+  
